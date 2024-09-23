6a800e55de3259bd817e7c801f997e5385cc1d3f
mohumadkhald <mohumadkhald@gmail.com>
2024-09-23 10:07:12 +0300

    end service registry

--- job-service/src/main/java/org.springframework.samples.jobservice/job/JobServiceImpl.java
+++ job-service/src/main/java/org.springframework.samples.jobservice/job/JobServiceImpl.java
@@ -1,8 +1,5 @@
 package org.springframework.samples.jobservice.job;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,22 +8,22 @@ import java.util.*;
 @Service
 public class JobServiceImpl implements JobService {
 
-  private static final Logger log = LoggerFactory.getLogger(JobServiceImpl.class);
-  private final JobRepository jobRepository;
 
-  public JobServiceImpl(JobRepository jobRepository) {
+  private final JobRepository jobRepository;
+  private final RestTemplate restTemplate;
+
+  public JobServiceImpl(JobRepository jobRepository, RestTemplate restTemplate) {
     this.jobRepository = jobRepository;
+    this.restTemplate = restTemplate;
   }
 
   @Override
   public List<JobDto> findJobs() {
-    RestTemplate restTemplate = new RestTemplate();
-
     // 1. Fetch all jobs
     return jobRepository.findAll().stream()
       .map(job -> {
         // 2. Fetch the company for each job
-        String companyUrl = "http://localhost:8082/companies/" + job.getCompanyId();
+        String companyUrl = "http://COMPANY-SERVICE:8082/companies/" + job.getCompanyId();
         Company company = restTemplate.getForObject(companyUrl, Company.class);
 
         // 3. Map the Job and Company to JobDto
@@ -37,8 +34,7 @@ public class JobServiceImpl implements JobService {
 
   @Override
   public boolean addJob(Job job) {
-    RestTemplate restTemplate = new RestTemplate();
-    Company[] companiesArray = restTemplate.getForObject("http://localhost:8082/companies", Company[].class);
+    Company[] companiesArray = restTemplate.getForObject("http://COMPANY-SERVICE:8082/companies", Company[].class);
     Long companyId = job.getCompanyId();
     assert companiesArray != null;
     for (Company company : companiesArray) {
@@ -53,9 +49,8 @@ public class JobServiceImpl implements JobService {
   @Override
   public JobDto findJob(long id) {
    Job job = jobRepository.findById(id).orElse(null);
-    RestTemplate restTemplate = new RestTemplate();
     assert job != null;
-    String companyUrl = "http://localhost:8082/companies/" + job.getCompanyId();
+    String companyUrl = "http://COMPANY-SERVICE:8082/companies/" + job.getCompanyId();
     Company company = restTemplate.getForObject(companyUrl, Company.class);
    return JobMapper.toJobDto(job, company);
   }
