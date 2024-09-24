cc09369f19325cfa2294048ba13be712823f6315
mohumadkhald <mohumadkhald@gmail.com>
2024-09-24 09:21:02 +0300

    add admin server and make data

---
+++ company-service/src/main/resources/postgres/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO companies (name, description, rating) VALUES
+                                                    ('Tech Solutions', 'A leading tech solutions provider.', 4.5),
+                                                    ('Green Energy Corp', 'Specializes in renewable energy solutions.', 4.8),
+                                                    ('Finance Group', 'Financial consulting and investment planning.', 4.2);
