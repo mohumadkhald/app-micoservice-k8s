f8c96a64ec62064661080e15b7f7e0fd93f19868
mohumadkhald <74939878+mohumadkhald@users.noreply.github.com>
2024-10-23 17:06:30 +0300

    Delete first directory

--- first/src/main/java/org/springframework/samples/first/company/CompanyRepository.java
+++
@@ -1,6 +0,0 @@
-package org.springframework.samples.first.company;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-}
